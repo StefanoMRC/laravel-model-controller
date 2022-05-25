96bbf6e15ec5c27897ae7ab9c55845e89614b1e8
StefanoMRC <stefano.mrc92@gmail.com>
2022-05-25 15:54:47 +0200

    model e controller

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Movie;
+
+class MovieController extends Controller
+{
+    public function index(){
+        
+        $movies=Movie::all();
+        dd($movies);
+        return view('home', compact('movies'));
+        
+    }
+}
